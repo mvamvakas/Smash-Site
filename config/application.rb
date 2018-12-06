b54ce1515658c315bc30cd3ed32a1ebd16925115
mvamvakas <michael@vamvakas.com>
2018-12-06 15:23:21 -0500

    idk man

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module SmashSite
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take
+    config.assets.initialize_on_precompile = false
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
